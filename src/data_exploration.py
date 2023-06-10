7ba34a7b72fa43ee2d4ecf1c56dcd7f62b7e9a83
kri78568 <kristina.lietz@item.fraunhofer.de>
2023-06-10 14:15:24 +0200

    pricing plot

--- src/data_exploration.py
+++ src/data_exploration.py
@@ -8,6 +8,7 @@ from datetime import datetime
 from utils import save_multi_image
 
 save = True
+ny_data = True
 
 ##################################################
 # DATA CITYLEARN CHALLENGE 2022
@@ -18,6 +19,10 @@ weather = pd.read_csv('citylearn/data/test/weather.csv')
 #normalize carbon intensity between 0 and 1
 carbon_intensity = pd.read_csv('citylearn/data/test/carbon_intensity.csv')['kg_CO2/kWh']
 carbon_intensity = (carbon_intensity - np.min(carbon_intensity)) / (np.max(carbon_intensity) - np.min(carbon_intensity))
+
+#normalize prices between 0 and 1
+pricing = pd.read_csv('citylearn/data/test/pricing.csv')['Electricity Pricing [$]']
+#pricing = (pricing - np.min(pricing)) / (np.max(pricing) - np.min(pricing))
 
 #normalize direct solar radiation between 0 and 1
 direct_solar = np.array(weather['Direct Solar Radiation [W/m2]'])
@@ -49,49 +54,68 @@ ax5.set_title('Diffuse solar radiation vs. direct solar radiation')
 ax5.set_xlabel('Diffuse solar radiation [$W/M^2$]')
 ax5.set_ylabel('Direct solar radiation [$W/M^2$]')
 
+fig1, ax1 = plt.subplots()
+ax1.plot(xs[:168], pricing[0:168])
+ax1.set_title('Electricity Pricing of one week')
+ax1.set_ylabel('Electricity Price [$]')
+ticks = pd.Series(pd.date_range(start=f'2021-01-01', end=f'2021-01-01 23:00:00', freq='H')).dt.time.astype(str).tolist()*7
+ticks = ['Mo']*24
+ticks.extend(['Tue']*24)
+ticks.extend(['Wed']*24)
+ticks.extend(['Thur']*24)
+ticks.extend(['Fri']*24)
+ticks.extend(['Sat']*24)
+ticks.extend(['Sun']*24)
+ax1.set_xticks(np.arange(0, len(ticks)))
+ax1.xaxis.set_tick_params(length=0)
+ax1.set_xticklabels(ticks, rotation=0)
+[l.set_visible(False) for (i,l) in enumerate(ax1.get_xticklabels()) if (i-18) % 24 != 0]
+
 
 ##################################################
 # REAL DATA FROM NEW YORK ISO AND NREL
 ##################################################
 
-fuel_files = os.listdir('../datasets/fuel_mix_ny')
-fuel = pd.read_csv('../datasets/fuel_mix_ny/' + fuel_files[0])
-fuel = (fuel.pivot_table(index=['Time Stamp'],
-                         columns='Fuel Category',
-                         values='Gen MW').reset_index().rename_axis(None, axis=1))
+if ny_data:
+    dir = '../datasets/fuel_mix_ny_2021'
+    fuel_files = os.listdir(dir)
+    fuel = pd.read_csv(f'{dir}/{fuel_files[0]}')
+    fuel = (fuel.pivot_table(index=['Time Stamp'],
+                             columns='Fuel Category',
+                             values='Gen MW').reset_index().rename_axis(None, axis=1))
 
-for file in fuel_files[1:]:
-    new = pd.read_csv('../datasets/fuel_mix_ny/' + file)
-    new = (new.pivot_table(index=['Time Stamp'],
-                           columns='Fuel Category',
-                           values='Gen MW').reset_index().rename_axis(None, axis=1))
-    fuel = fuel.append(new)
-fuel['Time Stamp'] = pd.to_datetime(fuel['Time Stamp'], format='%m/%d/%Y %H:%M:%S')
+    for file in fuel_files[1:]:
+        new = pd.read_csv(f'{dir}/{file}')
+        new = (new.pivot_table(index=['Time Stamp'],
+                               columns='Fuel Category',
+                               values='Gen MW').reset_index().rename_axis(None, axis=1))
+        fuel = fuel.append(new)
+    fuel['Time Stamp'] = pd.to_datetime(fuel['Time Stamp'], format='%m/%d/%Y %H:%M:%S')
 
-ny_weather = pd.read_csv('../datasets/weather_ny_42.30_-74.37_2021.csv', skiprows=2)
-ny_weather['Time Stamp'] = pd.to_datetime(ny_weather[['Month', 'Day', 'Year', 'Hour', 'Minute']]
-                                          .astype(str).apply(' '.join, 1), format='%m %d %Y %H %M')
-ny_weather = ny_weather.drop(columns=['Month', 'Day', 'Year', 'Hour', 'Minute'])
+    ny_weather = pd.read_csv('../datasets/weather_ny_42.30_-74.37_2021.csv', skiprows=2)
+    ny_weather['Time Stamp'] = pd.to_datetime(ny_weather[['Month', 'Day', 'Year', 'Hour', 'Minute']]
+                                              .astype(str).apply(' '.join, 1), format='%m %d %Y %H %M')
+    ny_weather = ny_weather.drop(columns=['Month', 'Day', 'Year', 'Hour', 'Minute'])
 
-joined = pd.merge(fuel, ny_weather, on='Time Stamp', sort=True)
+    joined = pd.merge(fuel, ny_weather, on='Time Stamp', sort=True)
 
-fig, ax = plt.subplots()
-ax.scatter(joined['Wind Speed'], joined['Wind'], s=1)
-ax.set_title('Wind energy vs. wind speed')
-ax.set_ylabel('Wind energy [$MW$]')
-ax.set_xlabel('Wind speed [$m/s$]')
+    fig, ax = plt.subplots()
+    ax.scatter(joined['Wind Speed'], joined['Wind'], s=1)
+    ax.set_title('Wind energy vs. wind speed')
+    ax.set_ylabel('Wind energy [$MW$]')
+    ax.set_xlabel('Wind speed [$m/s$]')
 
-fig, ax = plt.subplots()
-ax.scatter(joined['DHI'], joined['Other Renewables'], s=1)
-ax.set_title('Other renewables energy vs. DHI')
-ax.set_ylabel('Other renewables energy [$MW$]')
-ax.set_xlabel('DHI [$W/m^2$]')
+    fig, ax = plt.subplots()
+    ax.scatter(joined['DHI'], joined['Other Renewables'], s=1)
+    ax.set_title('Other renewables energy vs. DHI')
+    ax.set_ylabel('Other renewables energy [$MW$]')
+    ax.set_xlabel('DHI [$W/m^2$]')
 
-fig, ax = plt.subplots()
-ax.scatter(joined['DNI'],joined['Other Renewables'], s=1)
-ax.set_title('Other renewables energy vs. DNI')
-ax.set_ylabel('Other renewables energy [$MW$]')
-ax.set_xlabel('DNI [$W/m^2$]')
+    fig, ax = plt.subplots()
+    ax.scatter(joined['DNI'],joined['Other Renewables'], s=1)
+    ax.set_title('Other renewables energy vs. DNI')
+    ax.set_ylabel('Other renewables energy [$MW$]')
+    ax.set_xlabel('DNI [$W/m^2$]')
 
 if save:
     filename = "../datasets/data_exploration_plots/exploration-plots_" + datetime.now().strftime("%Y%m%dT%H%M%S")
