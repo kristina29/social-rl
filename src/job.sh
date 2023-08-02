277a3e8b7f20b70f7d58405f6969edce5178ce49
kri78568 <kristina.lietz@item.fraunhofer.de>
2023-08-02 18:30:44 +0200

    changed job

--- src/job.sh
+++ src/job.sh
@@ -18,6 +18,6 @@ source $HOME/.bashrc
 # insert your commands here
 #eval "$(micromamba shell hook --shell=bash)"
 micromamba activate social-rl
-srun python3 src/socialrl.py -s nydata -b 7 -d 2 -e 2 --tql --batch 1024
+srun python3 src/socialrl.py -s nydata -b 7 -d 2 -e 2 --tql --autotune
 micromamba deactivate
 
