09678177d0982988c194b554aeb030b86704efd5
kri78568 <kristina.lietz@item.fraunhofer.de>
2023-08-02 08:19:20 +0200

    new job

--- experiments/SAC_DB2/22_increase_batch_size/1024_autotune/old_buildings/job.sh
+++ experiments/SAC_DB2/22_increase_batch_size/1024_autotune/old_buildings/job.sh
@@ -18,6 +18,6 @@ source $HOME/.bashrc
 # insert your commands here
 #eval "$(micromamba shell hook --shell=bash)"
 micromamba activate social-rl
-srun python3 src/socialrl.py -s nydata_new_buildings2 -b 6 -d 2 -e 2 --tql --autotune --batch 1024
+srun python3 src/socialrl.py -s nydata -b 6 -d 2 -e 2 --tql --autotune --batch 1024
 micromamba deactivate
 
