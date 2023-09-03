4afa6cd07da825810860319fa4d3f17f67f3a396
kri78568 <kristina.lietz@item.fraunhofer.de>
2023-09-03 19:06:35 +0200

    new job

--- src/evaluation_plots.py
+++ src/evaluation_plots.py
@@ -20,12 +20,11 @@ if __name__ == '__main__':
         agent = parser.agent
 
     if True:
-        experiment_dirs = ['16_weather_8locs/standard_buildings',
+        experiment_dirs = ['23_autotune/old_buildings',
                            '26_l2loss/old_buildings',
-                           '16_weather_8locs/new_buildings2',
+                           '23_autotune/new_buildings2',
                            '26_l2loss/new_buildings2']
-        ref_dirs = ['16_weather_8locs/standard_buildings',
-                    '16_weather_8locs/new_buildings2']
+        ref_dirs = ['23_autotune/old_buildings', '23_autotune/new_buildings2']
         n_refs = len(ref_dirs)
         length = 1/n_refs
         agentdir = 'SAC_DB2'
