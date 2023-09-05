116de80aae6101e0f95d9b33a2e429cea1e6b6ad
kri78568 <kristina.lietz@item.fraunhofer.de>
2023-09-05 10:35:28 +0200

    longer training

--- src/socialrl.py
+++ src/socialrl.py
@@ -88,7 +88,8 @@ def train_sacdb2(schema, episodes, random_seed, batch_size, discount, autotune_e
     env = CityLearnEnv(schema)
     sacdb2_model = SACDB2(env=env, seed=random_seed, batch_size=batch_size, autotune_entropy=autotune_entropy,
                           clip_gradient=clip_gradient, kaiming_initialization=kaiming_initialization, l2_loss=l2_loss,
-                          discount=discount)
+                          discount=discount,
+                          start_training_time_step=1, end_exploration_time_step=7000)
     losses, rewards = sacdb2_model.learn(episodes=episodes, deterministic_finish=True)
 
     print('SAC DB2 model trained!')
