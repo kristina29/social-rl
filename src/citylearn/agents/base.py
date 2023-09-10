8dc4a385ecb0cf43530ea5aec89619f6e881e746
kri78568 <kristina.lietz@item.fraunhofer.de>
2023-09-10 15:03:48 +0200

    new job

--- src/citylearn/agents/base.py
+++ src/citylearn/agents/base.py
@@ -150,9 +150,9 @@ class Agent(Environment):
                         '1 - used_pv_of_total_share': []}
 
         # evaluate once a month for a whole week
-        if hasattr(self, 'start_training_time_step'):
-            print('start_training_time_step', self.start_training_time_step)
-            print('end_exploration_time_step', self.end_exploration_time_step)
+        #if hasattr(self, 'start_training_time_step'):
+            #print('start_training_time_step', self.start_training_time_step)
+            #print('end_exploration_time_step', self.end_exploration_time_step)
 
         for episode in range(episodes):
             deterministic = deterministic or (deterministic_finish and episode >= episodes - 1)
@@ -186,9 +186,9 @@ class Agent(Environment):
                 observations = [o for o in next_observations]
 
                 # evaluate once a month for a whole week
-                if hasattr(self, 'start_training_time_step') \
-                        and self.start_training_time_step <= self.time_step <= self.end_exploration_time_step:
-                    print('self.env.time_step % 168', self.env.time_step % 168)
+                #if hasattr(self, 'start_training_time_step') \
+                #        and self.start_training_time_step <= self.time_step <= self.end_exploration_time_step:
+                    #print('self.env.time_step % 168', self.env.time_step % 168)
                 if self.env.time_step % 168 == 0 \
                         and hasattr(self, 'start_training_time_step') \
                         and self.start_training_time_step <= self.time_step <= self.end_exploration_time_step:
@@ -207,15 +207,15 @@ class Agent(Environment):
                         kpis['value'] = kpis['value'].round(3)
                         kpis = kpis.rename(columns={'cost_function': 'kpi'})
                         kpis = kpis[kpis['level'] == 'district'].copy()
-                        print('KPIS', kpis)
+                        #print('KPIS', kpis)
 
                         for kpi, value in zip(kpis['kpi'], kpis['value']):
-                            print('KPI, value', kpi, value)
+                            #print('KPI, value', kpi, value)
                             if isinstance(value, float):
                                 eval_results[kpi].append(value)
                             else:
                                 eval_results[kpi].extend(value)
-                        print('')
+                        #print('')
 
                 logging.debug(
                     f'Time step: {self.env.time_step}/{self.env.time_steps - 1},' \
@@ -224,8 +224,8 @@ class Agent(Environment):
                     f' Rewards: {new_rewards}'
                 )
 
-                print('self.env.time_step', self.env.time_step)
-                print('env.done', self.env.done)
+                #print('self.env.time_step', self.env.time_step)
+                #print('env.done', self.env.done)
 
             # store episode's env to disk
             if keep_env_history:
@@ -234,7 +234,7 @@ class Agent(Environment):
                 pass
 
 
-        print(eval_results)
+        #print(eval_results)
         return losses, rewards, eval_results
 
     def get_env_history(self, directory: Path, episodes: List[int] = None):
