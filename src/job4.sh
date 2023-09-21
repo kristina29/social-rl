4d058f74234abc61a878ea5f942bc616fd0b2a77
kri78568 <kristina.lietz@item.fraunhofer.de>
2023-09-21 18:01:58 +0200

    new jobs

--- src/job4.sh
+++ src/job4.sh
@@ -19,7 +19,7 @@ source $HOME/.bashrc
 #eval "$(micromamba shell hook --shell=bash)"
 micromamba activate social-rl
 
-srun python3 src/socialrl.py -s nydata_new_buildings2 -b 6 --pretrained_demonstrator agents/SAC_agent_20230914T144400.pkl -e 2 --tql --sac --autotune --mode 4 --ir 1 --deterministic_demo
+srun python3 src/socialrl.py -s nydata_new_buildings2 -b 6 -d 4 -e 2 --tql --sac --autotune --mode 4 --ir 0.2 --deterministic_demo
 
 micromamba deactivate
 
--- src/scripts/generate_job_file.py
+++ src/scripts/generate_job_file.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-ir = 1
+ir = 0.2
 
 PREFIX = '''\
 #!/bin/bash
@@ -33,6 +33,6 @@ for mode in range(1,7):
     with open(f'job{mode}.sh', 'w') as rsh:
         rsh.write(f'''\
 {PREFIX}
-srun python3 src/socialrl.py -s nydata_new_buildings2 -b 6 --pretrained_demonstrator agents/SAC_agent_20230914T144400.pkl -e 2 --tql --sac --autotune --mode {mode} --ir {ir} --deterministic_demo
+srun python3 src/socialrl.py -s nydata_new_buildings2 -b 6 -d 4 -e 2 --tql --sac --autotune --mode {mode} --ir {ir} --deterministic_demo
 {SUFFIX}
     ''')
