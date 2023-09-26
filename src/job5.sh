d7f5dab9fa6d9d0d20dcb268b1969898f8f288a3
kri78568 <kristina.lietz@item.fraunhofer.de>
2023-09-26 13:09:42 +0200

    progress

--- src/job5.sh
+++ src/job5.sh
@@ -19,7 +19,8 @@ source $HOME/.bashrc
 #eval "$(micromamba shell hook --shell=bash)"
 micromamba activate social-rl
 
-srun python3 src/socialrl.py -s nydata_new_buildings2 -b 6 -d 4 -e 2 --tql --sac --autotune --mode 5 --ir 0.2 --deterministic_demo
+srun python3 src/socialrl.py -s nydata_new_buildings2 -b 6 -d 4 -e 2 --tql --sac --autotune --mode 5 --ir 0.01 --deterministic_demo
+
 
 micromamba deactivate
 
