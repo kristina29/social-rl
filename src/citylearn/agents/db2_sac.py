8ea13021c570744e74623094a8f35a58b59fcd0c
kri78568 <kristina.lietz@item.fraunhofer.de>
2023-09-26 16:03:13 +0200

    new clips

--- src/citylearn/agents/db2_sac.py
+++ src/citylearn/agents/db2_sac.py
@@ -126,7 +126,7 @@ class SACDB2(SAC):
                         policy_loss = (self.alpha[i] * log_pi - q_demonstrator).mean()
 
                         # prevent numerical errors
-                        policy_loss = policy_loss.clip(-1e+4, 1e+4)
+                        policy_loss = policy_loss.clip(-1e+3, 1e+3)
 
                         self.policy_optimizer[i].zero_grad()
                         policy_loss.backward()
