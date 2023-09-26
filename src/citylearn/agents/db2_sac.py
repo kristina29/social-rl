837264b7fb876df23dec49578fa7f250a32e6045
kri78568 <kristina.lietz@item.fraunhofer.de>
2023-09-26 13:08:06 +0200

    changed limits

--- src/citylearn/agents/db2_sac.py
+++ src/citylearn/agents/db2_sac.py
@@ -125,7 +125,7 @@ class SACDB2(SAC):
                         policy_loss = (self.alpha[i] * log_pi - q_demonstrator).mean()
 
                         # prevent numerical errors
-                        policy_loss = policy_loss.clip(-1000, 1000)
+                        policy_loss = policy_loss.clip(-1e+15, 1e+15)
 
                         self.policy_optimizer[i].zero_grad()
                         policy_loss.backward()
