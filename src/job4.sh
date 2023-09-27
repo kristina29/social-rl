4974da318f04636a64dc06a5c9f6dec5c89cf30f
kri78568 <kristina.lietz@item.fraunhofer.de>
2023-09-27 15:30:22 +0200

    added job files

--- src/job4.sh
+++ src/job4.sh
@@ -19,7 +19,7 @@ source $HOME/.bashrc
 #eval "$(micromamba shell hook --shell=bash)"
 micromamba activate social-rl
 
-srun python3 src/socialrl.py -s nydata_new_buildings2 -b 6 -d 2 -e 2 --tql --sac --sacdb2 --autotune --ir 0.02 --extra_policy_update
+srun python3 src/socialrl.py -s nydata_new_buildings2 -b 6 -d 4 -e 2 --tql --sac --sacdb2 --autotune --ir 0.02 --extra_policy_update
 
 micromamba deactivate
 
