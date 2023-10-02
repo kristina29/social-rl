aa259b14516bf0464f845812021d5cdb6487e1c7
kri78568 <kristina.lietz@item.fraunhofer.de>
2023-10-02 13:29:39 +0200

    new jobs

--- src/job6.sh
+++ src/job6.sh
@@ -19,7 +19,7 @@ source $HOME/.bashrc
 #eval "$(micromamba shell hook --shell=bash)"
 micromamba activate social-rl
 
-srun python3 src/socialrl.py -s nnb_limitobs1 -b 6 -d 4 -e 2 --tql --sac --sacdb2value --autotune --mode 6 --ir 0.01
+srun python3 src/socialrl.py -s nnb_limitobs1 -b 6 -d 4 -e 2 --tql --sac --sacdb2value --autotune --mode 6 --ir 0.2
 
 micromamba deactivate
 
