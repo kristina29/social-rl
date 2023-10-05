5077cccae652f49b057f4fbcce6b1e1417229d3f
kri78568 <kristina.lietz@item.fraunhofer.de>
2023-10-05 08:36:56 +0200

    new jobs

--- src/job3.sh
+++ src/job3.sh
@@ -19,7 +19,7 @@ source $HOME/.bashrc
 #eval "$(micromamba shell hook --shell=bash)"
 micromamba activate social-rl
 
-srun python3 src/socialrl.py -s nnb_limitobs1 -b 6 -d 2 -e 2 --tql --sac --sacdb2value --autotune --mode 3 --ir 1.5
+srun python3 src/socialrl.py -s nnb_limitobs1 -b 6 -d 4 -e 2 --tql --sac --sacdb2value --autotune --mode 3 --ir 1.5
 
 micromamba deactivate
 
--- src/scripts/generate_job_file.py
+++ src/scripts/generate_job_file.py
@@ -35,6 +35,6 @@ for mode in range(1,4):
     with open(f'job{mode}.sh', 'w') as rsh:
         rsh.write(f'''\
 {PREFIX}
-srun python3 src/socialrl.py -s nnb_limitobs1 -b 6 -d 2 -e 2 --tql --sac --sacdb2value --autotune --mode {mode} --ir {ir}
+srun python3 src/socialrl.py -s nnb_limitobs1 -b 6 -d 4 -e 2 --tql --sac --sacdb2value --autotune --mode {mode} --ir {ir}
 {SUFFIX}
     ''')
