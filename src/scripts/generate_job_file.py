2b8c8e25fcc2f04860fb09be885e1486e558e29e
kri78568 <kristina.lietz@item.fraunhofer.de>
2023-10-05 08:41:29 +0200

    new jobs

--- src/scripts/generate_job_file.py
+++ src/scripts/generate_job_file.py
@@ -35,6 +35,6 @@ for mode in range(1,4):
     with open(f'job{mode}.sh', 'w') as rsh:
         rsh.write(f'''\
 {PREFIX}
-srun python3 src/socialrl.py -s nnb_limitobs1 -b 6 --pretrained_demonstrator agents/SAC_agent_Building5.pkl -e 2 --tql --sac --sacdb2value --autotune --mode {mode} --ir {ir}
+srun python3 src/socialrl.py -s nnb_limitobs1 -b 6 --pretrained_demonstrator agents/SAC_agent_Building6.pkl -e 2 --tql --sac --sacdb2value --autotune --mode {mode} --ir {ir}
 {SUFFIX}
     ''')
