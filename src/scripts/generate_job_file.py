28eaab646d366aa3f356b6407f930563e11557b5
kri78568 <kristina.lietz@item.fraunhofer.de>
2023-10-05 15:33:56 +0200

    added jobs

--- src/scripts/generate_job_file.py
+++ src/scripts/generate_job_file.py
@@ -35,6 +35,6 @@ for i, ir in enumerate(irs):
     with open(f'job{i}.sh', 'w') as rsh:
         rsh.write(f'''\
 {PREFIX}
-srun python3 src/socialrl.py -s nnb_limitobs1 -b 6 -d 4 -e 2 --tql --sac --sacdb2 --autotune --ir {ir} --only_demo_action
+srun python3 src/socialrl.py -s nnb_limitobs1 -b 6 --pretrained_demonstrator agents/SAC_agent_Building5.pkl -e 2 --tql --sac --sacdb2 --autotune --ir {ir} --only_demo_action
 {SUFFIX}
     ''')
