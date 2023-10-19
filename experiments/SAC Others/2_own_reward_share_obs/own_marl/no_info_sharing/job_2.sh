3a9898a04c1c7e1b503c1e365df6873c01064e5f
kri78568 <kristina.lietz@item.fraunhofer.de>
2023-10-19 13:18:04 +0200

    new jobs

--- experiments/SAC Others/2_own_reward_share_obs/own_marl/no_info_sharing/job_2.sh
+++ experiments/SAC Others/2_own_reward_share_obs/own_marl/no_info_sharing/job_2.sh
@@ -19,8 +19,7 @@ source $HOME/.bashrc
 #eval "$(micromamba shell hook --shell=bash)"
 micromamba activate social-rl
 
-srun python3 src/nonsocialrl.py -s nnb_limitobs1 -b 6 -e 2 --tql --autotune 
+srun python3 src/marlisa_social.py -s nnb_limitobs1_marlisa -b 6 -e 2 --autotune
 
 micromamba deactivate
 
-    
