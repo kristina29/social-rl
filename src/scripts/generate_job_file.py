9d244040be8cfcdf58fc191c55141f899df2a19f
kri78568 <kristina.lietz@item.fraunhofer.de>
2023-10-29 19:30:23 +0100

    new jobs

--- src/scripts/generate_job_file.py
+++ src/scripts/generate_job_file.py
@@ -35,6 +35,6 @@ for i, ir in enumerate(irs):
     with open(f'job{i}.sh', 'w') as rsh:
         rsh.write(f'''\
 {PREFIX}
-srun python3 src/socialrl.py -s nnb_limitobs1_shared -b 6 --pretrained_demonstrator agents/SAC_agent_Building6.pkl -e 2 --tql --sac --sacdb2 --autotune --ir {ir} --extra_policy_update --deterministic_demo
+srun python3 src/socialrl.py -s nnb_limitobs1_shared -b 6 --pretrained_demonstrator agents/SAC_agent_Building6.pkl -e 2 --tql --sac --sacdb2 --autotune --ir {ir} --deterministic_demo
 {SUFFIX}
     ''')
