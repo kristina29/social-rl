61e2b9d207ddfb427e1f309db147ca9c8c05eb01
kri78568 <kristina.lietz@item.fraunhofer.de>
2023-11-10 00:23:16 +0100

    bla

--- src/scripts/loss_value_plots.py
+++ src/scripts/loss_value_plots.py
@@ -6,37 +6,54 @@ from matplotlib import pyplot as plt
 
 plt.rcParams.update({
         "text.usetex": True,
+        'text.latex.preamble': r"\usepackage{bm}"
     })
+
 
 def plot_losses(losses, include_alpha) -> List[plt.Figure]:
 
-    fig, (ax, ax2) = plt.subplots(2,1, figsize=(14,9), sharex=True)
+    fig, (ax, ax2) = plt.subplots(1,2, figsize=(14,7))
     #ax2 = ax.twinx()
     i = 0
     for env_name, env_values in losses.items():
         for nn_name, nn_values in env_values.items():
-            if nn_name == 'policy_losses':
-                ax.plot(np.arange(len(nn_values)), nn_values, label=env_name)
-            if include_alpha[i] and nn_name == 'alpha_vals':
-                ax2.plot(np.arange(len(nn_values)), nn_values, label=f'{env_name} (alpha vals)', linewidth=2, linestyle= (5, (10, 3)))
-        i += 1
+            if i == 0:
+                ax.set_ylabel(env_name, fontsize=21, fontdict=dict(weight='bold'))
+                if nn_name == 'policy_losses':
+                    ax.plot(np.arange(len(nn_values)), nn_values, label='Policy loss', zorder=3)
+                    i += 1
+                if nn_name == 'q1_losses':
+                    ax.plot(np.arange(len(nn_values)), nn_values, label='Q-Network loss', zorder=3)
+            elif i == 1:
+                ax2.set_ylabel(env_name, fontsize=21)
+                if nn_name == 'policy_losses':
+                    ax2.plot(np.arange(len(nn_values)), nn_values, label='Policy loss', zorder=3)
+                if nn_name == 'q1_losses':
+                    ax2.plot(np.arange(len(nn_values)), nn_values, label='Q-Network loss', zorder=3)
 
-    ax.set_ylabel(f'Policy loss value', fontsize=21)
-    ax2.set_ylabel(r'Entropy $\alpha$', fontsize=21, labelpad=15)
+    ax.set_title('a)', fontsize=21, pad=10)
+    ax2.set_title('b)', fontsize=21, pad=10)
+    ax.axhline(0, color='black', zorder=2)
+    ax2.axhline(0, color='black', zorder=2)
+    ax.set_xlabel('Time step', fontsize=21)
     ax2.set_xlabel('Time step', fontsize=21)
+    ax.tick_params(axis='x', which='both', labelsize=21)
     ax2.tick_params(axis='x', which='both', labelsize=21)
     ax.tick_params(axis='y', which='both', labelsize=21)
     ax2.tick_params(axis='y', which='both', labelsize=21)
     #ax.set_title(f'{env_name}')
-    ax.grid(axis='y')
-    ax2.grid(axis='y')
+    ax.grid(axis='y', zorder=1)
+    ax2.grid(axis='y', zorder=1)
 
     handles, labels = ax.get_legend_handles_labels()
     #handles.append(Line2D([0], [0], label=r'$\alpha$', color='black', linewidth=2, linestyle= (5, (10, 3))))
 
-    fig.legend(handles=handles, bbox_to_anchor=(0.074, 0.99),
-               loc='upper left', fontsize=21)
-    plt.tight_layout()
+    plt.tight_layout(rect=[0, 0.15, 1, 1])
+    fig.legend(handles=handles, #bbox_to_anchor=(0.5, 0.1),
+               loc='lower center', fontsize=21, framealpha=0)
+
+    plt.subplots_adjust(wspace=0.3)
+
 
     fig.savefig('losses.pdf')
     #plt.show()
@@ -44,13 +61,14 @@ def plot_losses(losses, include_alpha) -> List[plt.Figure]:
 
 if __name__ == '__main__':
     loss_files = [#'../experiments/SAC_DB2/30_renewable_prod/reward_05pvprice/0.5/losses_20231002T130931.pkl',
-                  '../experiments/SAC_DB2/32_demo_b5/ir0.2/socialMode2/losses_20231107T200708.pkl',
-                  '../experiments/SAC_DB2/32_demo_b5/ir0.2/socialMode5/losses_20231107T201820.pkl' ]
+                  '../experiments/SAC_DB2Value/9_interchanged_observations/demo_b6_determ/extra_pol_update/ir0.15/losses_20231029T201710.pkl',
+                  '../experiments/SAC_DB2Value/4_demo_b6_policyupdate/ir0.0001/losses_20231003T110826.pkl' ]
     agents = [#'SAC',
-         'SAC_DB2', 'SAC_DB2']
+         'SAC_DB2Value', 'SAC_DB2Value']
+
     names = [#'x',
-         'Mode 2', 'Mode 5']
-    include_alpha = [True, True]
+         r'$\bm{\alpha_i=0.15}$', r'$\bm{\alpha_i=1e^{-4}}$']
+    include_alpha = [False, False]
 
     final_losses = {}
 
